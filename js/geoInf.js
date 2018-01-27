69e4c104f9441b01f31a7a2421b452d221114974
kellypala <kelly.pala@heig-vd.ch>
2018-01-27 10:36:16 +0100

    Màj

--- js/geoInf.js
+++ js/geoInf.js
@@ -3,7 +3,7 @@ $(function () {
     mapboxgl.accessToken = 'pk.eyJ1Ijoia3BhbGEiLCJhIjoiY2picWYxcXFlMDJzcDMycjZ4YTluN3V0ayJ9.CkFeFkTB1oVk8-BXK27uSw';
     var map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/kpala/cjcx3co3d0s2q2rs1xrum5ah2',
+        style: 'mapbox://styles/kpala/cjcx4dst00svm2spfn9ixmulj',
         center: [6.65, 46.7833333],
         zoom: 10,
     }); 
